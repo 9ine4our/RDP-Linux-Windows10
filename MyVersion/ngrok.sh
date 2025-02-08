522e201181465ca3af3712fa1d5b075ffbe97655
9ine4our <148853314+9ine4our@users.noreply.github.com>
2025-02-08 14:55:59 -0800

    Update ngrok.sh

--- MyVersion/ngrok.sh
+++ MyVersion/ngrok.sh
@@ -8,5 +8,5 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-tar -xvzf ngrok.tgz > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
